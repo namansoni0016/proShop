b44cc92c18d3a4c2dd9415a61b9ddf69f5b93358
Naman Soni <namantsoni@gmail.com>
2024-02-21 12:12:33 +0530

    Redux

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,20 +1,17 @@
-import { useEffect, useState } from 'react';
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import axios from 'axios';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([]);
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products');
-            setProducts(data);
-        };
-        fetchProducts();
-    }, []);
+    const { data : products, isLoading, error } = useGetProductsQuery();
     return (
         <>
+            { isLoading ? (
+                <Loader/>
+            ) : error ? (<Message variant='danger'>{ error?.data?.message || error.error }</Message>) : (<>
             <h1>Latest Products</h1>
             <Row>
                 {products.map((product) => (
@@ -23,6 +20,7 @@ const HomeScreen = () => {
                     </Col>
                 ))}
             </Row>
+            </>) }
         </>
     ) 
 }
