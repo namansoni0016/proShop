84afa2168cb03be33ad645372d9b60b39976b3f3
Naman Soni <namantsoni@gmail.com>
2024-03-11 19:20:51 +0530

    Product Carousel

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,11 +5,13 @@ import { getProducts,
     createProduct, 
     updateProduct, 
     deleteProduct, 
-    createProductReview
+    createProductReview,
+    getTopProducts
 } from "../controller/productController.js";
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
+router.get('/top', getTopProducts);
 router.route('/:id').get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
 router.route('/:id/reviews').post(protect, createProductReview);
 
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -6,6 +6,7 @@ import Product from '../components/Product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import Paginate from '../components/Paginate';
+import ProductCarousel from '../components/ProductCarousel';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 const HomeScreen = () => {
@@ -13,7 +14,7 @@ const HomeScreen = () => {
     const { data, isLoading, error } = useGetProductsQuery({ keyword, pageNumber});
     return (
         <>
-            {keyword && <Link to='/' className='btn btn-light mb-4'>Go Back</Link>}
+            {!keyword ? <ProductCarousel/> : <Link to='/' className='btn btn-light mb-4'>Go Back</Link>}
             { isLoading ? (
                 <Loader/>
             ) : error ? (<Message variant='danger'>{ error?.data?.message || error.error }</Message>) : (<>
