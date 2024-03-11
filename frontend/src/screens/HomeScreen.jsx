0374b44a5044e44671ed05bacd1685e4d22eba8a
Naman Soni <namantsoni@gmail.com>
2024-03-11 12:48:01 +0530

    Search Functionality

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import Product from '../components/Product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
@@ -8,10 +9,11 @@ import Paginate from '../components/Paginate';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 const HomeScreen = () => {
-    const { pageNumber } = useParams();
-    const { data, isLoading, error } = useGetProductsQuery({ pageNumber});
+    const { pageNumber, keyword  } = useParams();
+    const { data, isLoading, error } = useGetProductsQuery({ keyword, pageNumber});
     return (
         <>
+            {keyword && <Link to='/' className='btn btn-light mb-4'>Go Back</Link>}
             { isLoading ? (
                 <Loader/>
             ) : error ? (<Message variant='danger'>{ error?.data?.message || error.error }</Message>) : (<>
@@ -23,7 +25,7 @@ const HomeScreen = () => {
                     </Col>
                 ))}
             </Row>
-            <Paginate pages={data.pages} page={data.page}/> 
+            <Paginate pages={data.pages} page={data.page} keyword= {keyword ? keyword : ''}/> 
             </>) }
         </>
     ) 
